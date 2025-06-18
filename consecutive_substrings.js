21aca7ad2e118a2a72c826bdf3f0979a75e49fd2
Michael McCormack <mcctwin@gmail.com>
2025-06-17 19:07:53 -0500

    function added, pseudocode added

--- consecutive_substrings.js
+++ consecutive_substrings.js
@@ -1,6 +1,13 @@
 function consecutiveSubstrings(string) {
-  // type your code here
+  let result = [];
+  for (let i = 0; i < string.length; i++) {
+    for (let j = i + 1; j <= string.length; j++) {
+      result.push(string.slice(i, j));
+    }
+  }
+  return result;
 }
+console.log(consecutiveSubstrings('abc'));
 
 if (require.main === module) {
   // add your own tests in here
